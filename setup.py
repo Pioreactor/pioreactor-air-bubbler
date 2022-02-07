017386904ab7baed024110f50f82021a6ba62a88
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-02-07 12:29:51 -0500

    small fix for ui

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pioreactor_air_bubbler",
-    version="0.1.0",
+    version="0.1.1",
     license="MIT",
     description="Add an air bubbler to your Pioreactor as a background job",
     long_description=open("README.md").read(),
